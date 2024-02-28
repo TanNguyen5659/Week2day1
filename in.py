6fe8a9415664f1cb21ab3086f21dd4ea9b8140a3
Tan Nguyen <tannguyen5659@gmail.com>
2024-02-27 20:48:04 -0800

    first

--- in.py
+++ in.py
@@ -1,5 +1,9 @@
-while True:
-    print('playing game')
-    playing = input("Are you still playing?: ")
-    if playing == "no":
-        break
+l_1 = [1,2,3,4,5,6]
+l_2 = [3,4,5,6,7,8,10]
+
+def merge_sort(list1,list2):
+    list3 = list1 + list2
+    list3.sort()
+    return list3
+    
+print(merge_sort(l_1,l_2))
